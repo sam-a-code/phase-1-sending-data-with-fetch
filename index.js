0c6a337731d062e387867d0cbe70c38e2fd4f238
Sam Avery <sam.avery1@gmail.com>
2022-09-07 11:34:33 -0500

    commit tree bb061c2b0ec1fbdb7c32d2aaf84b368483185198

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData (name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json",
+    },
+    body: JSON.stringify({name, email})
+    })
+    .then(res => res.json())
+    .then(object => {document.body.textContent = object["id"];})
+    .catch(error => {document.body.textContent = error.message})
+}
+
+// const configurationObject = {
+// }
+
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response){
+//         return response.json();
+//     })
+//     .then(function (object){
+//         console.log(object);
+//     })
+//     .catch(function(error){
+//         alert("Bad things!");
+//         console.log(error.message)
+//     })
